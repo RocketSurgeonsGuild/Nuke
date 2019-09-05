868741a056979a5d30a270a3888c1b8765485670
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-09-04 21:16:46 -0500

    Fix MacBuild to use nuget restore

--- src/Nuke.Xamarin/FormsBuild.cs
+++ src/Nuke.Xamarin/FormsBuild.cs
@@ -5,8 +5,14 @@ using Nuke.Common;
 
 namespace Rocket.Surgery.Nuke.Xamarin
 {
+    /// <summary>
+    /// Base build plan for Xamarin.Forms based applications
+    /// </summary>
     public class FormsBuild : XamarinBuild
     {
+        /// <summary>
+        /// Core target that can be used to trigger all targets for this build
+        /// </summary>
         public Target XamarinForms => _ => _;
     }
 }
--- src/Nuke.Xamarin/MacBuild.cs
+++ src/Nuke.Xamarin/MacBuild.cs
@@ -2,23 +2,38 @@ using System;
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Tools.NuGet;
 using Rocket.Surgery.Nuke;
 
 namespace Rocket.Surgery.Nuke.Xamarin
 {
-    [CheckBuildProjectConfigurations]
-    [UnsetVisualStudioEnvironmentVariables]
-    public class MacBuild : XamarinBuild
+    /// <summary>
+    /// Base build plan for Xamarin.Mac based applications
+    /// </summary>
+    public abstract class MacBuild : XamarinBuild
     {
+        /// <summary>
+        /// Core target that can be used to trigger all targets for this build
+        /// </summary>
         public Target XamarinMac => _ => _
             .DependsOn(Clean)
             .DependsOn(Restore)
             .DependsOn(Build);
 
+        /// <inheritdoc />
+        public override Target Restore => _ => _
+            .DependsOn(Clean)
+            .Executes(() =>
+            {
+                NuGetTasks
+                    .NuGetRestore(settings =>
+                        settings
+                            .SetSolutionDirectory(Solution)
+                            .EnableNoCache());
+            });
 
-        /// <summary>
-        /// msbuild
-        /// </summary>
+
+        /// <inheritdoc />
         public override Target Build => _ => _
             .DependsOn(Restore)
             .Executes(() =>
