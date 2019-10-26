e16589019982da441cad26906a8ea9fe80f78d29
David Driscoll <david.driscoll@gmail.com>
2019-10-25 23:56:13 -0400

    Updated rbb

--- src/Nuke/RocketBoosterBuild.cs
+++ src/Nuke/RocketBoosterBuild.cs
@@ -133,7 +133,7 @@ namespace Rocket.Surgery.Nuke
         public Target BuildVersion => _ => _
             .Executes(() =>
             {
-                GitVersionTasks.GitVersion(x => x.SetOutput(GitVersionOutput.buildserver));
+                GitVersionTasks.GitVersion(x => x.SetFramework("netcoreapp3.0").SetOutput(GitVersionOutput.buildserver));
                 Logger.Info("Building version {0} of {1} ({2}) using version {3} of Nuke.",
                     GitVersion.FullSemVer,
                     Solution.Name,
