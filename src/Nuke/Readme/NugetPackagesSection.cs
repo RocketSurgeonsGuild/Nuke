12d538c3eee9c686dea16c98009241505c084dcd
David Driscoll <david.driscoll@gmail.com>
2020-08-11 02:03:58 -0400

    Updated nuget packages references to use PackageId

--- src/Nuke/Readme/NugetPackagesSection.cs
+++ src/Nuke/Readme/NugetPackagesSection.cs
@@ -1,3 +1,4 @@
+using Nuke.Common.ProjectModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -90,7 +91,7 @@ namespace Rocket.Surgery.Nuke.Readme
             IHaveSolution build
         )
         {
-            var packageNames = build.Solution.WherePackable().Select(x => x.Name);
+            var packageNames = build.Solution.WherePackable().Select(x => x.GetProperty<string>("PackageId") ?? x.Name);
 
             var sb = new StringBuilder();
             if (config.ContainsKey("myget"))
