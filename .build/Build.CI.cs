c9e47c2674b7e0219b8c37ca733be9265d6329a3
David Driscoll <david.driscoll@gmail.com>
2020-11-09 23:37:24 -0500

    Updated build ci (#304)

--- .build/Build.CI.cs
+++ .build/Build.CI.cs
@@ -66,6 +66,9 @@ public partial class Solution
             new SetupDotNetStep("Use .NET Core 3.1 SDK") {
                 DotNetVersion = "3.1.x"
             },
+            new SetupDotNetStep("Use .NET Core 5.0 SDK") {
+                DotNetVersion = "5.0.100-rc.2.20479.15"
+            },
         });
 
         buildJob.Steps.Add(new UsingStep("Publish Coverage")
