447942400d3774f3809e0b2375af1c8787e30691
David Driscoll <david.driscoll@gmail.com>
2023-11-19 23:16:16 -0500

    maybe it'll lint?

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -20,7 +20,7 @@ function cleanupcode(filenames) {
 // MoveUnshippedToShipped
 
 module.exports = {
-    '!(*verified|*received).cs': filenames => [`dotnet nuke lint --lint-files ${filenames.join(' ')}`],
+    '!(*verified|*received).cs': filenames => [`.build/bin/Debug/net6.0/.build.exe lint --lint-files ${filenames.join(' ')}`],
     '*.{csproj,targets,props,xml}': filenames => forEachChunk(filenames, chunk => [`prettier --write '${chunk.join(`' '`)}'`]),
     '*.{js,ts,jsx,tsx,json,yml,yaml}': filenames => forEachChunk(filenames, chunk => [`prettier --write '${chunk.join(`' '`)}'`]),
 };
