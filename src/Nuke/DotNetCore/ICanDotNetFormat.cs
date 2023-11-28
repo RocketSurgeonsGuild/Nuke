0bf5e4aaf93651c7e9f3c2524754cce6fd47b33a
David Driscoll <david.driscoll@gmail.com>
2023-11-27 20:20:22 -0500

    Updated to latest configuration

--- src/Nuke/DotNetCore/ICanDotNetFormat.cs
+++ src/Nuke/DotNetCore/ICanDotNetFormat.cs
@@ -11,7 +11,7 @@ public interface ICanDotNetFormat : IHaveSolution, ICanLint
     /// <summary>
     ///     The default severity to use for DotNetFormat
     /// </summary>
-    public DotNetFormatSeverity DotNetFormatSeverity => DotNetFormatSeverity.warn;
+    public DotNetFormatSeverity DotNetFormatSeverity => DotNetFormatSeverity.info;
 
     /// <summary>
     ///     The default profile to use for JetBrainsCleanupCode
@@ -22,37 +22,37 @@ public interface ICanDotNetFormat : IHaveSolution, ICanLint
     ///     The dotnet format target
     /// </summary>
     public Target DotNetFormat => d =>
-        d
-           .TriggeredBy(PostLint)
-           .After(Lint)
-           .OnlyWhenStatic(() => IsLocalBuild || LintPaths.Any())
-           .Executes(
-                () => LintPaths.Any()
-                    ? DotNetTasks.DotNet($"format --severity {DotNetFormatSeverity} --include {string.Join(",", LintPaths)}")
-                    : DotNetTasks.DotNet($"format --severity {DotNetFormatSeverity}")
-            );
+                                      d
+                                         .TriggeredBy(PostLint)
+                                         .After(Lint)
+                                         .OnlyWhenStatic(() => IsLocalBuild || LintPaths.Any())
+                                         .Executes(
+                                              () => LintPaths.Any()
+                                                  ? DotNetTasks.DotNet($"format --severity {DotNetFormatSeverity} --include {string.Join(",", LintPaths)}")
+                                                  : DotNetTasks.DotNet($"format --severity {DotNetFormatSeverity}")
+                                          );
 
     /// <summary>
     ///     Use the jetbrains code cleanup tool to format the code if installed
     /// </summary>
     public Target JetBrainsCleanupCode => d =>
-        d
-           .TriggeredBy(PostLint)
-           .After(DotNetFormat)
-           .OnlyWhenStatic(() => IsLocalBuild || LintPaths.Any())
-           .OnlyWhenStatic(() => DotNetTool.IsInstalled("jb"))
-           .Executes(
-                () => LintPaths.Any()
-                    ? DotNetTasks.DotNet(
-                        $""""jb cleanupcode "{Solution.Path}" --profile={JetBrainsCleanupCodeProfile} --disable-settings-layers="GlobalAll;GlobalPerProduct;SolutionPersonal;ProjectPersonal" --include={string.Join(";", LintPaths.Select(z => RootDirectory.GetRelativePathTo(z)))} """",
-                        RootDirectory,
-                        logOutput: true
-                    )
-                    : DotNetTasks.DotNet(
-                        $""""jb cleanupcode "{Solution.Path}" --profile={JetBrainsCleanupCodeProfile} --disable-settings-layers="GlobalAll;GlobalPerProduct;SolutionPersonal;ProjectPersonal" """",
-                        RootDirectory,
-                        logOutput: true
-                    )
-            );
+                                              d
+                                                 .TriggeredBy(PostLint)
+                                                 .After(DotNetFormat)
+                                                 .OnlyWhenStatic(() => IsLocalBuild || LintPaths.Any())
+                                                 .OnlyWhenStatic(() => DotNetTool.IsInstalled("jb"))
+                                                 .Executes(
+                                                      () => LintPaths.Any()
+                                                          ? DotNetTasks.DotNet(
+                                                              $""""jb cleanupcode "{Solution.Path}" --profile={JetBrainsCleanupCodeProfile} --disable-settings-layers="GlobalAll;GlobalPerProduct;SolutionPersonal;ProjectPersonal" --include={string.Join(";", LintPaths.Select(z => RootDirectory.GetRelativePathTo(z)))} """",
+                                                              RootDirectory,
+                                                              logOutput: true
+                                                          )
+                                                          : DotNetTasks.DotNet(
+                                                              $""""jb cleanupcode "{Solution.Path}" --profile={JetBrainsCleanupCodeProfile} --disable-settings-layers="GlobalAll;GlobalPerProduct;SolutionPersonal;ProjectPersonal" """",
+                                                              RootDirectory,
+                                                              logOutput: true
+                                                          )
+                                                  );
     // --include
 }
