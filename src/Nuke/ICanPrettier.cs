e435bb357a0d4408e430538c4f333b9a2da89ae9
David Driscoll <david.driscoll@gmail.com>
2024-09-04 22:49:42 -0400

    ensure prettier is installed

--- src/Nuke/ICanPrettier.cs
+++ src/Nuke/ICanPrettier.cs
@@ -34,6 +34,11 @@ public interface ICanPrettier : ICanLint
                              ? makeArgsForStagedFiles(LintPaths.Glob(PrettierMatcher))
                              : new Arguments().Concatenate(_prettierBaseArgs).Add(".").Add("--write");
 
+                     ProcessTasks
+                        .StartProcess(ToolPathResolver.GetPathExecutable("npm"), NukeBuild.IsLocalBuild ? "install" : "ci --ignore-scripts", NukeBuild.RootDirectory)
+                        .AssertWaitForExit()
+                        .AssertZeroExitCode();
+
                      return ProcessTasks
                            .StartProcess(
                                 ToolPathResolver.GetPathExecutable("npm"),
