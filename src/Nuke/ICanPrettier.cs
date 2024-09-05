57e10cd04e126573c428dc9e7c259ed3383794db
David Driscoll <david.driscoll@gmail.com>
2024-09-04 22:52:32 -0400

    ifs

--- src/Nuke/ICanPrettier.cs
+++ src/Nuke/ICanPrettier.cs
@@ -34,10 +34,13 @@ public interface ICanPrettier : ICanLint
                              ? makeArgsForStagedFiles(LintPaths.Glob(PrettierMatcher))
                              : new Arguments().Concatenate(_prettierBaseArgs).Add(".").Add("--write");
 
-                     ProcessTasks
-                        .StartProcess(ToolPathResolver.GetPathExecutable("npm"), NukeBuild.IsLocalBuild ? "install" : "ci --ignore-scripts", NukeBuild.RootDirectory)
-                        .AssertWaitForExit()
-                        .AssertZeroExitCode();
+                     if (( NukeBuild.RootDirectory / "package.json" ).FileExists() && !NukeBuild.RootDirectory.ContainsDirectory("node_modules"))
+                     {
+                         ProcessTasks
+                            .StartProcess(ToolPathResolver.GetPathExecutable("npm"), NukeBuild.IsLocalBuild ? "install" : "ci --ignore-scripts", NukeBuild.RootDirectory)
+                            .AssertWaitForExit()
+                            .AssertZeroExitCode();
+                     }
 
                      return ProcessTasks
                            .StartProcess(
