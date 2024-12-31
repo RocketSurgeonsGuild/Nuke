82ca23ce775bd76b9e9e4b86603479db87f1414e
Rocket Understudy <33589210+rsg-bot@users.noreply.github.com>
2024-12-31 12:11:14 -0500

    Automatically linting code

--- src/Nuke/TitleEventsAttribute.cs
+++ src/Nuke/TitleEventsAttribute.cs
@@ -83,12 +83,12 @@ public sealed class TitleEventsAttribute : BuildExtensionAttributeBase, IOnBuild
         After:
                 Convert.ToInt16(NukeBuild.IsServerBuild ? 0 : Math.Round((double)step++ / ( (short)plan.Count + 1 ) * 100));
         */
-/* Unmerged change from project 'Rocket.Surgery.Nuke(net9.0)'
-Before:
-        Convert.ToInt16(NukeBuild.IsServerBuild ? 0 : Math.Round(( (double)step++ / ( (short)plan.Count + 1 ) ) * 100));
-After:
-        Convert.ToInt16(NukeBuild.IsServerBuild ? 0 : Math.Round((double)step++ / ( (short)plan.Count + 1 ) * 100));
-*/
+        /* Unmerged change from project 'Rocket.Surgery.Nuke(net9.0)'
+        Before:
+                Convert.ToInt16(NukeBuild.IsServerBuild ? 0 : Math.Round(( (double)step++ / ( (short)plan.Count + 1 ) ) * 100));
+        After:
+                Convert.ToInt16(NukeBuild.IsServerBuild ? 0 : Math.Round((double)step++ / ( (short)plan.Count + 1 ) * 100));
+        */
         Convert.ToInt16(NukeBuild.IsServerBuild ? 0 : Math.Round(( (double)step++ / ( (short)plan.Count + 1 ) ) * 100));
 
     private IReadOnlyCollection<ExecutableTarget> _plan = [];
